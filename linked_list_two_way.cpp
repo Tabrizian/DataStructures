fceef096acbd040db7c28eb12628eb1c7093af7e
Iman Tabrizian <tabrizian@outlook.com>
2015-10-28 18:02:57 +0330

    Update linked_list_two_way.cpp

--- linked_list_two_way.cpp
+++ linked_list_two_way.cpp
@@ -7,7 +7,7 @@ struct node
 {
     int data;
     struct node *fp;
-	struct node *bp;
+    struct node *bp;
 };
 
 struct node* create_node(int data)
@@ -15,7 +15,7 @@ struct node* create_node(int data)
     struct node *result = (struct node *) malloc(sizeof(node));
     result->data = data;
     result->bp = NULL;
-	result->fp = NULL;
+    result->fp = NULL;
     return result;
 }
 
@@ -25,7 +25,7 @@ void add_end(struct node * first, int data)
     for(current=first; current->fp!=NULL; current=current->fp);
     struct node *new_node = create_node(data);
     current->fp = new_node;
-	new_node->bp = current; 
+    new_node->bp = current;
 }
 
 void print_list(struct node *first)
@@ -37,82 +37,118 @@ void print_list(struct node *first)
     }
 }
 
-void print_list_reverse(struct node *first){
-	struct node *current;
-	for(current=first;current->fp!=NULL;current=current->fp);
-	for(;current!=NULL;current=current->bp){
-		cout<<current->data<<endl;
-	}
+void print_list_reverse(struct node *first)
+{
+    struct node *current;
+    for(current=first; current->fp!=NULL; current=current->fp);
+    for(; current!=NULL; current=current->bp)
+    {
+        cout<<current->data<<endl;
+    }
 
 }
-//
+
 void add_front(struct node** first,int data)
 {
     struct node* new_node = create_node(data);
     new_node->fp =*first;
-	(*first)->bp = new_node;
+    (*first)->bp = new_node;
     *first = new_node;
 }
-//
-//void delete_end(struct node* first)
-//{
-//    struct node* current;
-//    for(current = first; current->link->link!=NULL; current=current->link);
-//    free(current->link);
-//    current->link=NULL;
-//}
-//
-//void delete_front(struct node** first)
-//{
-//    struct node* new_first = (*first)->link;
-//    free(*first);
-//    *first = new_first;
-//}
-//
-//void insert_next_node(struct node* current_node,int data)
-//{
-//    struct node* new_node = create_node(data);
-//    struct node* next = current_node->link;
-//    current_node->link = new_node;
-//    new_node->link = next;
-//}
-//void reverse_list(struct node** first)
-//{
-//    struct node* neighbs[3] = {NULL, NULL,*first};
-//    while(neighbs[2])
-//    {
-//        neighbs[1] = neighbs[2];
-//        neighbs[2]=neighbs[2]->link;
-//        neighbs[1]->link = neighbs[0];
-//        neighbs[0] = neighbs[1];
-//
-//    }
-//
-//    *first = neighbs[1];
-//}
-//void remove_item(struct node** first,int data)
-//{
-//    struct node* current = *first;
-//    if(current->data == data)
-//    {
-//        delete_front(first);
-//        current = *first;
-//    }
-//    for(current; current->link!=NULL; current=current->link)
-//    {
-//        if(current->link->data == data)
-//        {
-//            struct node *next = current->link->link;
-//            free(current->link);
-//            current->link = next;
-//            if(current->link == NULL)
-//                return;
-//        }
-//
-//    }
-//
-//}
-//
+
+void delete_end(struct node* first)
+{
+    struct node* current;
+    for(current = first; current->fp->fp!=NULL; current=current->fp);
+    free(current->fp);
+    current->fp=NULL;
+}
+
+void delete_front(struct node** first)
+{
+    struct node* new_first = (*first)->fp;
+    free(*first);
+    *first = new_first;
+    new_first->bp = NULL;
+}
+
+void insert_next_node(struct node* current_node,int data)
+{
+    struct node* new_node = create_node(data);
+    new_node->bp = current_node;
+    new_node->fp = current_node->fp;
+    new_node->fp->bp = new_node;
+    new_node->bp->fp = new_node;
+
+}
+
+void insert_before_node(struct node* current_node,int data)
+{
+    struct node* new_node = create_node(data);
+    new_node->fp = current_node;
+    new_node->bp = current_node->bp;
+    new_node->fp->bp = new_node;
+    new_node->bp->fp = new_node;
+}
+void swap_node(struct node *a)
+{
+    struct node *temp;
+    temp = a->fp;
+    a->fp = a->bp;
+    a->bp = temp;
+}
+void reverse_list(struct node** first)
+{
+    struct node *current = *first;
+    while(current->fp!=NULL)
+    {
+        struct node *next = current->fp;
+        swap_node(current);
+        current = next;
+    }
+    swap_node(current);
+    *first = current;
+
+}
+void reverse_list_recursive(struct node** first)
+{
+    if((*first)->fp==NULL)
+    {
+        swap_node(*first);
+        return;
+    }
+
+    struct node *next = (*first)->fp;
+    swap_node(*first);
+    *first = next;
+    reverse_list_recursive(first);
+}
+
+void remove_item(struct node** first,int data)
+{
+    struct node* current = *first;
+    if(current->data == data)
+    {
+        delete_front(first);
+        current = *first;
+    }
+    for(current; current->fp!=NULL; current=current->fp)
+    {
+        if(current->fp->data == data)
+        {
+            struct node *next = current->fp->fp;
+            free(current->fp);
+            current->fp = next;
+            if(next!=NULL)
+                next->bp = current;
+            if(current->fp == NULL)
+                return;
+        }
+
+    }
+
+}
+
 //struct node* merge_lists(struct node *first_x,struct node *first_y)
 //{
 //    if(first_x == NULL) return first_y;
@@ -150,17 +186,17 @@ void add_front(struct node** first,int data)
 //    return result;
 //
 //}
+
 int main()
 {
-    struct node* first = create_node(10);
+    struct node* first = create_node(80);
     add_end(first,18);
     add_end(first,80);
     add_end(first,90);
     add_end(first,180);
-    add_end(first,190);
-	add_front(&first,5);
-	cout<<first->fp->bp->data<<endl;
-	int x;
-	cin>>x;
+    add_end(first,10);
+    remove_item(&first,10);
+    print_list(first);
     return 0;
 }
+
