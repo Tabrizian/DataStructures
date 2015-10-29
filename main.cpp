eee9f0fa9a2d4329b693b7acf0819d987c3f9ca1
Iman Tabrizian <tabrizian@outlook.com>
2015-10-29 22:48:42 +0330

    Nothing important!

--- main.cpp
+++ main.cpp
@@ -14,14 +14,15 @@ struct node* create_node(int data)
 {
     struct node *result = (struct node *) malloc(sizeof(node));
     result->data = data;
-    result->link = NULL;
+    result->fp = NULL;
+    result->fp = NULL;
     return result;
 }
 
+
 int main()
 {
-    struct node* first_y = create_node(10);
-    struct node* first_x = create_node(15);
+    struct node* first_x = create_node(10);
     print_list(merge_lists(first_y,first_x));
     return 0;
 }
